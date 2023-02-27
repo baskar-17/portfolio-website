9e7a86da446eb6be1adb3cec880a0799a8e1cde8
Baskar <baskar.official@outlook.in>
2023-02-28 00:12:12 +0530

    1.2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./app.css";
 import { Link, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 import Home from "./components/home/Home";
 import Blog from "./components/blog/Blog";
@@ -10,24 +11,48 @@ import Guestbook from "./components/guestbook/Guestbook";
 import logo from "./assets/logo.svg";
 
 function App() {
+  let [switchTo, newSwitchTo] = useState(1);
+
+  function switchTab(index) {
+    newSwitchTo(index);
+  }
+
   return (
     <div className="container">
-      <div className="logo">
-        <img src={logo} alt="logo baskar portfolio" />
-      </div>
-      <div className="home-navigator">
-        <Link className="nav-link active" to={"/"}>
-          home
-        </Link>
-        <Link className="nav-link" to={"/about"}>
-          about
-        </Link>
-        <Link className="nav-link" to={"/blog"}>
-          blog
-        </Link>
-        <Link className="nav-link" to={"/guestbook"}>
-          guestbook
-        </Link>
+      <div>
+        <div className="logo">
+          <img src={logo} alt="logo baskar portfolio" />
+        </div>
+        <div className="home-navigator">
+          <Link
+            className={switchTo === 1 ? "nav-link active" : "nav-link"}
+            onClick={() => switchTab(1)}
+            to={"/"}
+          >
+            home
+          </Link>
+          <Link
+            className={switchTo === 2 ? "nav-link active" : "nav-link"}
+            onClick={() => switchTab(2)}
+            to={"/about"}
+          >
+            about
+          </Link>
+          <Link
+            className={switchTo === 3 ? "nav-link active" : "nav-link"}
+            onClick={() => switchTab(3)}
+            to={"/blog"}
+          >
+            blog
+          </Link>
+          <Link
+            className={switchTo === 4 ? "nav-link active" : "nav-link"}
+            onClick={() => switchTab(4)}
+            to={"/guestbook"}
+          >
+            guestbook
+          </Link>
+        </div>
       </div>
 
       <Routes>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./home.model.css";
+import { FaLinkedinIn, FaGithub, FaBroom } from "react-icons/fa";
+import { FiArrowUpRight } from "react-icons/fi";
 
 import profile from "../../assets/baskar.png";
 
@@ -8,9 +10,10 @@ function Home() {
     <div>
       <div className="home-content-container">
         <h1>Baskar</h1>
-        <p>
-          Hey, I'm Lee. I'm the VP of Developer Experience at Vercel where my
-          team helps developers build a faster web.
+        <p className="home-content-para">
+          Hey, I'm Baskar. I'm an
+          <strong> Aspiring frontend Web Developer with UI/UX Skills. </strong>
+          where my team helps developers build a faster web.
         </p>
 
         <div className="home-profile-container">
@@ -18,13 +21,19 @@ function Home() {
             <img src={profile} alt="Baskar Portfolio Profile" />
           </div>
           <div className="home-stats">
-            <p>6,873 tweets all time</p>
-            <p>6,873 stars on this repo</p>
-            <p>1,609,184 blog views all time</p>
+            <p>
+              <FaLinkedinIn size={20} /> 6,873 tweets all time
+            </p>
+            <p>
+              <FaGithub size={20} /> 6,873 stars on this repo
+            </p>
+            <p>
+              <FaBroom size={20} /> 1,609,184 blog views all time
+            </p>
           </div>
         </div>
 
-        <p>
+        <p className="home-profile-para">
           I'm a Next.js contributor and help lead our open-source communities.
           I'm passionate about frontend development and have created courses on
           React, Next.js, and web development. I'm an advisor and investor in
@@ -32,8 +41,12 @@ function Home() {
         </p>
 
         <div className="home-footer-links">
-          <a href="/">follow me on twitter</a>
-          <a href="/">get email updates</a>
+          <a href="/">
+            <FiArrowUpRight size={20} /> follow me on twitter
+          </a>
+          <a href="/">
+            <FiArrowUpRight size={20} /> get email updates
+          </a>
         </div>
       </div>
     </div>
