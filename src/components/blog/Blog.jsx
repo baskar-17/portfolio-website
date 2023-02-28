5c19a0a37986ff5a6dde2f0f19c69aebe050ecac
Baskar <baskar.official@outlook.in>
2023-02-28 23:52:22 +0530

    frontend Completed

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import "./blog.model.css";
 
 function Blog() {
   return (
-    <div>
+    <div className="blog-container">
       <h1>Blog</h1>
+      <div className="blog-list-container">
+        <a href="">
+          <div className="blog-list">
+            <p>Funding Open Source</p>
+            <p className="blog-number">13,998 views</p>
+          </div>
+        </a>
+
+        <a href="">
+          <div className="blog-list">
+            <p>
+              Building an Image Gallery with Next.js, Supabase, and Tailwind CSS
+            </p>
+            <p className="blog-number">13,998 views</p>
+          </div>
+        </a>
+
+        <a href="">
+          <div className="blog-list">
+            <p>Funding Open Source</p>
+            <p className="blog-number">13,998 views</p>
+          </div>
+        </a>
+
+        <a href="">
+          <div className="blog-list">
+            <p>Building an Image Gallery with Next.js, Supabase</p>
+            <p className="blog-number">13,998 views</p>
+          </div>
+        </a>
+
+        <a href="">
+          <div className="blog-list">
+            <p>Funding Open Source</p>
+            <p className="blog-number">13,998 views</p>
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/guestbook/Guestbook.jsx
+++ src/components/guestbook/Guestbook.jsx
@@ -1,9 +1,65 @@
 import React from "react";
+import "./guestbook.model.css";
+import { FaGithub } from "react-icons/fa";
 
 function Guestbook() {
   return (
-    <div>
+    <div className="guestbook-container">
       <h1>Guest Book</h1>
+
+      <a href="">
+        <div className="guestbook-social-btn">
+          <FaGithub className="icon" />
+          <p>Sign in with GitHub</p>
+        </div>
+      </a>
+
+      <div className="guestbook-list-container">
+        <p>
+          <span>User Name: </span>
+          big ups on your site and your code walkthrough of it. helped me out a
+          lot!
+        </p>
+
+        <p>
+          <span>User Name: </span>
+          This is really good.
+        </p>
+
+        <p>
+          <span>User Name: </span>
+          your work and Vercel's is amazing. hugs.
+        </p>
+
+        <p>
+          <span>User Name: </span>
+          Hi man, really love your work!
+        </p>
+
+        <p>
+          <span>User Name: </span>
+          🚀 awesome
+        </p>
+
+        <p>
+          <span>User Name: </span>I was here. You are awesome ✨
+        </p>
+
+        <p>
+          <span>User Name: </span>
+          Awesome
+        </p>
+
+        <p>
+          <span>User Name: </span>
+          Thank you. Amazing 👏
+        </p>
+
+        <p>
+          <span>User Name: </span>
+          woah. this is nice
+        </p>
+      </div>
     </div>
   );
 }
